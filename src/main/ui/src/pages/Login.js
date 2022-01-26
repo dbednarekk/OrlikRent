b02dfdaa13a2ee50e74cd2a3f48a56020f31ec5c
Damian Bednarek <damian.bednarekk@gmail.com>
2022-01-26 17:23:27 +0100

    implemented logout function, implemented basic showing in header login and access level

--- src/main/ui/src/pages/Login.js
+++ src/main/ui/src/pages/Login.js
@@ -12,7 +12,16 @@ function Login() {
     const handleError = useErrorHandler()
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
-   
+
+    function parseJwt (token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
+    };
     const handleAddUser = () => {
         const json = JSON.stringify({
             login,
@@ -26,6 +35,10 @@ function Login() {
             }
         }).then((res) => {
             sessionStorage.setItem("JWTToken", JSON.stringify(res.data))
+            const cred = parseJwt(res.data)
+            console.log(cred)
+            sessionStorage.setItem("Login",JSON.stringify(cred.sub))
+            sessionStorage.setItem("Auth",JSON.stringify(cred.auth))
             navigate('/');
           }).catch(error =>{
             const message = error.response.data
