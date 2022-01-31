ffce22a847aa10c1f2aea87aa62c0d8c15ee2b6a
224329 Michał Kłyż <224329@edu.p.lodz.pl>
2022-01-31 01:23:30 +0100

    Dodanie lissty rezerwacji dla boiska ale wyskakujer 403 forbiden

--- src/main/ui/src/pages/PopupDataPitch.tsx
+++ src/main/ui/src/pages/PopupDataPitch.tsx
@@ -6,7 +6,18 @@ import { useState, useEffect } from "react";
 import axios from "../Services/URL";
 import { If, Then } from 'react-if';
 import styletb from '../styles/tableStyle.module.css'
-
+import Box from "@mui/material/Box";
+import styles from "../styles/FootballPitch.module.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import ActiveIcon from "../components/ActiveIcon";
+import Autocomplete from "../components/Autocomplete";
+import TextField from "@mui/material/TextField";
 
 export interface PopupDataPitch {
     open: boolean,
@@ -16,6 +27,97 @@ export interface PopupDataPitch {
 }
 
 export default function PopupDataPitch({open, onCancel, id, pitch}){
+
+function Row(props) {
+    const { row } = props;
+
+    return (
+        <React.Fragment>
+            <TableRow>
+                <TableCell align="center" component="th" scope="row">
+                    {row.id}
+                </TableCell>
+                <TableCell align="center">{row.accountID}</TableCell>
+                <TableCell align="center">{row.pitchID}</TableCell>
+                <TableCell align="center">{row.start_date_rental}</TableCell>
+                <TableCell align="center">{row.end_date_rental}</TableCell>
+                <TableCell align="center">
+                    <ActiveIcon active={row.active} />
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
+
+function BasicTable() {
+    const [rentals, setRentals] = useState([]);
+    const getRentals = () => {
+        const token = sessionStorage.getItem("JWTToken")
+        return axios.get(`Rentals/RentsForPitch/${id}`,{
+            headers:{
+                'Authorization': `Bearer ${token}`
+            }})
+    }
+
+    const getUpdatedRentals = () => {
+        return getRentals().then((res) => {
+            setRentals(res.data);
+        })
+    };
+    useEffect(() => {
+        getUpdatedRentals();
+    }, []);
+    const rnts = [];
+    const [searchInput, setSearchInput] = useState("");
+
+    function search(rows) {
+        if (Array.isArray(rows) && rows.length) {
+            const filteredRent = rows.filter(
+                (row) =>
+                    row.props.row.id.concat(" ",row.props.row.accountID).concat(" ", row.props.row.pitchID).toLowerCase().indexOf(searchInput.toLowerCase()) > -1
+            );
+
+            filteredRent.forEach((rent) =>
+                rnts.includes(rent.props.row.id + " " + rent.props.row.accountID + " " + rent.props.row.pitchID) ? "" : rnts.push(rent.props.row.id)
+            );
+            return filteredRent;
+        } else {
+            return rows;
+        }
+    }
+    return (
+        <Box
+            style={{
+                position: "relative",
+                top: "20%",
+            }}
+        >
+            <TableContainer component={Paper} className={styles.table}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell />
+                            <TableCell align="center">id</TableCell>
+                            <TableCell align="center">account ID</TableCell>
+                            <TableCell align="center">pitch ID</TableCell>
+                            <TableCell align="center">start Date</TableCell>
+                            <TableCell align="center">end Date</TableCell>
+                            <TableCell align="center">active</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {search(
+                            rentals.map((row, index) => (
+                                <Row key={index} row={row} onChange={getUpdatedRentals} />
+                            ))
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+    );
+}
+
 
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
@@ -106,6 +208,7 @@ export default function PopupDataPitch({open, onCancel, id, pitch}){
                         <td className={styletb.tdData}><h2>{numberOfBaskets1}</h2></td>
                         </Then></If>
                     </tr>
+                    <BasicTable />
                 </Grid>
             </DialogContent>
             <DialogActions>
